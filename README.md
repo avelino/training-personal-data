27ba1678d708e87a91ef77f763787818cc53c81a
Avelino <31996+avelino@users.noreply.github.com>
2025-01-03 14:56:11 -0300

    simplify ouraring data fetching
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- README.md
+++ README.md
@@ -9,6 +9,6 @@
 **run:**
 
 ``` sh
-OURA_TOKEN=IUDVON2HW5NCUZ7GPULDRDILBGBVWHAI \
-    bb -cp src -m training-personal-data.core
+OURA_TOKEN=... \
+    bb -cp src -m training-personal-data.ouraring 2024-12-01 2024-12-01
 ```
--- src/training_personal_data/ouraring.clj
+++ src/training_personal_data/ouraring.clj
@@ -34,3 +34,24 @@
     (if (= status 200)
       body
       (throw (ex-info "Failed to get daily activity" {:status status :body body}))))) 
+
+(defn -main
+  "Main function that fetches daily activity data from Oura Ring API
+   for a given date range passed as command line arguments.
+   
+   Usage:
+   bb -m training-personal-data.ouraring 2024-01-01 2024-01-31
+   
+   Arguments:
+   - start-date: Start date in YYYY-MM-DD format
+   - end-date: End date in YYYY-MM-DD format
+   
+   Returns:
+   - Prints the raw API data
+   - Prints the path of the saved JSON file containing the data"
+  [& args]
+  (let [start-date (first args)
+        end-date (second args)
+        body (get-daily-activity start-date end-date)]
+    (println body)
+    (println "save file:" (save-to-json body start-date end-date))))
