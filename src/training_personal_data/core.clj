13541682b7d271ea2a90272b09c5f00ce4b9df8e
Avelino <31996+avelino@users.noreply.github.com>
2025-01-03 14:06:07 -0300

    ouraring: initial call to api
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/training_personal_data/core.clj
+++ src/training_personal_data/core.clj
@@ -2,7 +2,12 @@
   (:require [training-personal-data.ouraring :as ouraring]))
 
 (defn -main
-  "initial software here"
+  "Main function that fetches daily activity data from Oura Ring API
+   for a specific period (December 1-31, 2024) and saves it to a JSON file.
+   
+   Returns:
+   - Prints the raw API data
+   - Prints the path of the saved JSON file containing the data"
   [& _args]
   (let [start-date "2024-12-01"
         end-date "2024-12-31"
