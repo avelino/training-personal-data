0af0f71e4ceb62445013eaaa9ec2956c7d74f98e
Avelino <31996+avelino@users.noreply.github.com>
2025-02-03 06:43:06 -0300

    feat(wahoo): add support for fetching workouts by date range
    - Updated wahoo endpoint to fetch workouts by date range instead of single workout ID
    - Added environment variable for Wahoo API token
    - Updated bb.edn task documentation to reflect new date range parameters
    - Refactored workout fetching to support bulk date range queries
    
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/training_personal_data/wahoo.clj
+++ src/training_personal_data/wahoo.clj
@@ -1,11 +1,25 @@
 (ns training-personal-data.wahoo
   (:require [training-personal-data.wahoo.endpoints.workout.core :as workout]
             [training-personal-data.db :as db]
-            [training-personal-data.config :as config]))
+            [training-personal-data.config :as config]
+            [taoensso.timbre :as log]))
 
 (defn -main [& args]
-  (let [token (System/getenv "WAHOO_TOKEN")
-        start-date (first args)
-        end-date (second args)
-        db-spec (db/make-db-spec (config/get-db-config))]
-    (workout/fetch-and-save-by-date token start-date end-date db-spec)))
+  (try
+    (let [token (System/getenv "WAHOO_TOKEN")
+          start-date (first args)
+          end-date (second args)
+          db-spec (db/make-db-spec (config/get-db-config))]
+      (when (or (nil? start-date) (nil? end-date))
+        (throw (ex-info "Usage: bb -m training-personal-data.wahoo <start-date> <end-date>" {:args args})))
+      (when (nil? token)
+        (throw (ex-info "Missing WAHOO_TOKEN environment variable" {})))
+      (log/info {:event :start :msg "Starting Wahoo workout sync" :start start-date :end end-date})
+      (workout/fetch-and-save-by-date token start-date end-date db-spec)
+      (log/info {:event :complete :msg "Completed Wahoo workout sync"}))
+    (catch Exception e
+      (log/error {:event :error :msg (ex-message e) :data (ex-data e)})
+      (println "\n❌ Wahoo sync failed:" (ex-message e))
+      (when-let [data (ex-data e)]
+        (println "   Details:" data))
+      (System/exit 1))))
