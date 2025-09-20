9f6dd2ba2c22e968cd35061fe9709bfeffa170a3
Avelino <31996+avelino@users.noreply.github.com>
2025-09-19 22:57:51 -0300

    refactor: move Wahoo API logic to dedicated namespace
    - Created `training-personal-data.wahoo.api` for Wahoo API client functions (token refresh, auth code exchange, HTTP helpers)
    - Updated references in main Wahoo namespace and workout endpoint to use new API namespace
    - Improved code organization and maintainability for Wahoo integration
    - Updated AGENT.md to document Wahoo as a supported data source and clarify architecture
    - Updated .gitignore to include `.secrets/` for secure token storage
    
    No functional changes; this is a pure refactor for clarity and separation of concerns.
    
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/training_personal_data/wahoo/api.clj
+++ src/training_personal_data/wahoo/api.clj
@@ -5,6 +5,7 @@
             [taoensso.timbre :as log]))
 
 (def ^:private host "https://api.wahooligan.com/v1")
+(def ^:private oauth-token-url "https://api.wahooligan.com/oauth/token")
 
 (defn build-headers [token]
   {"Authorization" (str "Bearer " token)
@@ -18,6 +19,61 @@
       {:success? false
        :error {:status status
                :body body}})))
+
+(defn refresh-access-token
+  "Exchange a refresh token for a new access token (and refresh token).
+  Returns a map {:access_token ... :refresh_token ... :expires_in ... :scope ...} on success.
+  Throws ex-info on failure."
+  [client-id client-secret refresh-token]
+  (log/info {:event :oauth-refresh-start :msg "Refreshing Wahoo access token"})
+  (let [response (http/post oauth-token-url
+                            {:headers {"Content-Type" "application/x-www-form-urlencoded"}
+                             :form-params {"client_id" client-id
+                                           "client_secret" client-secret
+                                           "grant_type" "refresh_token"
+                                           "refresh_token" refresh-token}})
+        {:keys [status body]} response]
+    (if (= status 200)
+      (let [data (json/parse-string body true)
+            {:keys [refresh_token expires_in token_type]} data]
+        (log/info {:event :oauth-refresh-success
+                   :msg "Refreshed Wahoo access token"
+                   :expires_in expires_in
+                   :token_type token_type
+                   :has_refresh_token (boolean refresh_token)})
+        data)
+      (do
+        (log/error {:event :oauth-refresh-error
+                    :msg "Failed to refresh Wahoo access token"
+                    :status status
+                    :body body})
+        (throw (ex-info "Failed to refresh Wahoo access token"
+                        {:status status :body body}))))))
+
+(defn exchange-authorization-code
+  "Exchange an authorization code for access and refresh tokens.
+  Returns {:access_token :refresh_token :expires_in ...} or throws ex-info on failure."
+  [client-id client-secret code redirect-uri]
+  (log/info {:event :oauth-authcode-start :msg "Exchanging authorization code for tokens"})
+  (let [response (http/post oauth-token-url
+                            {:headers {"Content-Type" "application/x-www-form-urlencoded"}
+                             :form-params {"client_id" client-id
+                                           "client_secret" client-secret
+                                           "code" code
+                                           "redirect_uri" redirect-uri
+                                           "grant_type" "authorization_code"}})
+        {:keys [status body]} response]
+    (if (= status 200)
+      (let [data (json/parse-string body true)]
+        (log/info {:event :oauth-authcode-success :msg "Obtained tokens via authorization code"})
+        data)
+      (do
+        (log/error {:event :oauth-authcode-error
+                    :msg "Failed to exchange authorization code"
+                    :status status
+                    :body body})
+        (throw (ex-info "Failed to exchange authorization code"
+                        {:status status :body body}))))))
 
 (defn fetch-workouts-by-date [token start-date end-date]
   (log/info {:event :api-fetch :msg "Fetching workouts by date range"
--- src/training_personal_data/wahoo/endpoints/workout/api.clj
+++ src/training_personal_data/wahoo/endpoints/workout/api.clj
@@ -3,17 +3,30 @@
             [cheshire.core :as json]))
 
 (defn fetch [token start-date end-date]
-  (api/fetch-workouts-by-date token start-date end-date))
+  (let [resp (api/fetch-workouts-by-date token start-date end-date)]
+    (if (:success? resp)
+      (let [data (:data resp)
+            workouts (cond
+                       (map? data) (or (:workouts data)
+                                       (:data data)
+                                       (when (:workout data) [(:workout data)])
+                                       [])
+                       (sequential? data) data
+                       :else [])]
+        {:success? true :data workouts})
+      resp)))
 
 (defn normalize [workout]
-  {:id (:id workout)
-   :starts (:starts workout)
-   :minutes (:minutes workout)
-   :name (:name workout)
-   :plan_id (:plan_id workout)
-   :workout_token (:workout_token workout)
-   :workout_type_id (:workout_type_id workout)
-   :workout_summary (:workout_summary workout)
-   :created_at (:created_at workout)
-   :updated_at (:updated_at workout)
-   :raw_json (json/generate-string workout)})
+  (let [id (or (:id workout) (:workout_id workout))
+        plan-id (:plan_id workout)]
+    {:id (some-> id str)
+     :starts (:starts workout)
+     :minutes (:minutes workout)
+     :name (:name workout)
+     :plan_id (when plan-id (str plan-id))
+     :workout_token (:workout_token workout)
+     :workout_type_id (:workout_type_id workout)
+     :workout_summary (:workout_summary workout)
+     :created_at (:created_at workout)
+     :updated_at (:updated_at workout)
+     :raw_json (json/generate-string workout)}))
